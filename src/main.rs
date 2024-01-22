dcfd10411b9ee03998a7db59d16b8630661bffea
zhouhang95 <765229842@qq.com>
2024-01-23 05:08:20 +0800

    mj rgis

--- src/main.rs
+++ src/main.rs
@@ -141,7 +141,7 @@ fn main_mj_rgis() {
     }
     let _seperate_storage = file.read_u16::<LE>().unwrap();
     let _base_size = file.read_u16::<LE>().unwrap();
-    if true {
+    for _ in 0..anim_count {
         let _name = read_string(&mut file);
         let _anim_root_name = read_string(&mut file);
         let bone_count = file.read_u16::<LE>().unwrap();
@@ -162,45 +162,42 @@ fn main_mj_rgis() {
             dbg!(key_time);
         }
         println!("{:x}", file.position());
-        for _ in 0..key_count {
-            let mut content = [0u8; 16];
-            file.read(&mut content).unwrap();
-            println!("{:?}", content);
+        for i in 0..bone_count {
+            let has_pos_keys = file.read_u8().unwrap();
+            let has_rot_keys = file.read_u8().unwrap();
+            let has_scale_keys = file.read_u8().unwrap();
+            let euler_flags = file.read_u8().unwrap();
+            println!("{}: {:x} {} {} {} {}", i, file.position(), has_pos_keys, has_rot_keys, has_scale_keys, euler_flags);
+            assert_eq!(euler_flags, 0);
+            if has_pos_keys != 0{
+                for _ in 0..key_count {
+                    let trans = read_float3(&mut file);
+                }
+            } else {
+                read_float3(&mut file);
+            }
+            if has_rot_keys != 0 {
+                for _ in 0..key_count {
+                    let quat = read_half_quat(&mut file);
+                    println!("{:?}", quat);
+                }
+            } else {
+                let quat = read_half_quat(&mut file);
+            }
+            if has_scale_keys != 0 {
+                for _ in 0..key_count {
+                    read_half3(&mut file);
+                }
+            } else {
+                let s = read_half3(&mut file);
+                dbg!(s);
+            }
         }
-        println!("{:x}", file.position());
-    }
-    file.seek(std::io::SeekFrom::Start(0x427)).unwrap();
-    print!("------------");
-    if false {
-        let _name = read_string(&mut file);
-        let _anim_root_name = read_string(&mut file);
-        let bone_count = file.read_u16::<LE>().unwrap();
-        for _ in 0..bone_count {
-            read_string(&mut file);
-        }
-        let _sample_fps = file.read_u32::<LE>().unwrap();
-        let _is_loop = file.read_u8().unwrap() != 0;
-        let _has_scaled = file.read_u8().unwrap() != 0;
-        let _prs_flags = file.read_u16::<LE>().unwrap();
-        let _accum_flags = file.read_u32::<LE>().unwrap();
-        let _pack_prs_flags = file.read_u8().unwrap();
-        let _bone_separate_flags = file.read_u8().unwrap();
-        let key_count = file.read_u16::<LE>().unwrap();
-        dbg!(key_count);
-        println!("{:x}", file.position());
-        for _ in 0..key_count {
-            let key_time = file.read_f32::<LE>().unwrap();
-            dbg!(key_time);
-        }
-        for _ in 0..key_count {
-            let mut content = [0u8; 16];
-            file.read(&mut content).unwrap();
-            println!("{:?}", content);
-        }
+        let has_pivot_track = file.read_u8().unwrap() != 0;
         println!("{:x}", file.position());
     }
 }
 
 fn main() {
-    main_rgis_sub();
+    main_mj_rgis();
 }
